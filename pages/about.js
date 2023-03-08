621f10eb84fb888d8ed3ee21844f39994734e630
Amadeo Noels <amadeo@origen.studio>
2023-03-08 15:23:37 +0100

    remove getStaticProps

--- pages/about.js
+++ pages/about.js
@@ -2,11 +2,4 @@ export default function About() {
     return <div>About</div>
   }
 
-  export async function getStaticProps(context){
-    return {
-      redirect:{
-        destination: '/',
-        permanent: true
-      }
-    }
-  }
+  
